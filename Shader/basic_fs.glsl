69ffb0df621f24c6384c983b3f2c65147d4a3c3f
Lemonade <1395842642@qq.com>
2023-08-13 10:19:43 +0800

    使用正常方法绘制小行星带

--- Shader/basic_fs.glsl
+++ Shader/basic_fs.glsl
@@ -3,9 +3,9 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D texture1;
+uniform sampler2D texture_diffuse1;
 
 void main()
 {             
-    FragColor = texture(texture1, TexCoords);
+    FragColor = texture(texture_diffuse1, TexCoords);
 }
